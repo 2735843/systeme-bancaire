361d1b78705504d556ff8183779379b33c5d9cd9
Liticia ANZE <2735843@collegelacite.ca>
2026-03-07 13:20:25 -0500

    Retrait du code sur la branche

--- projetfinal/backend/src/config/jwt.js
+++
@@ -1,2 +0,0 @@
-export const JWT_SECRET = "banking_secret_key";
-export const JWT_EXPIRES = "1h";
--- projetfinal/backend/src/routes/beneficiary.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { requireRoles, verifyToken } from "../middlewares/auth.middleware.js";
-import { listBeneficiaries } from "../controllers/beneficiary.controller.js";
-
-const router = Router();
-
-router.get("/", verifyToken, requireRoles("client"), listBeneficiaries);
-
-export default router;
